282749b20f774113e1fb259810212872ee58c075
Junjie Yang <yangjunjie0320@gmail.com>
2024-05-22 20:50:06 -0700

    fix git action

--- CudaExtension/setup.py
+++ CudaExtension/setup.py
@@ -1,30 +1,14 @@
-# ./CxxExtension/setup.py
-import os
-from os import listdir
-from os.path import join, dirname, abspath
-
 from setuptools import setup
-
-from torch.utils.cpp_extension import BuildExtension
-from torch.utils.cpp_extension import CUDAExtension
-
-name = "sigmoid_with_cuda_extension"
-path = dirname(abspath(__file__))
-
-is_source_file = lambda s: s.endswith(".cxx") or s.endswith(".cu")
-sources = (lambda d: [join(d, s) for s in listdir(d) if is_source_file(s)])(
-    join(path, "src")
-)
-
-extension = CUDAExtension(
-    name, sources=sources,
-    include_dirs=[join(path, "include")],
-)
-
-cmdclass = {"build_ext": BuildExtension}
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(
-    name=name, version="0.1",
-    ext_modules=[extension],
-    cmdclass=cmdclass,
-)
+    name='mysigmoid2',
+    ext_modules=[
+        CUDAExtension('mysigmoid2', [
+            './src/MySigmoidKernel.cu',
+            './src/MySigmoidCUDA.cpp',
+        ]),
+    ],
+    cmdclass={
+        'build_ext': BuildExtension
+    })
